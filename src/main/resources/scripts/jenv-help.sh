094e1dabff9a9a3ceae095c9941bba1f67255390
Jacky Chan <libing.chen@gmail.com>
2013-02-17 11:24:15 +0800

    jenv help command

---
+++ src/main/resources/scripts/jenv-help.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+#   Copyright 2012 Jacky Chan
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+function __jenvtool_help {
+	echo ""
+	echo "Usage: jenv <command> <candidate> [version]"
+	echo ""
+	echo "   command    :  install, uninstall, list, use, current, version, default, selfupdate, broadcast or help"
+	echo "   candidate  :  $(echo "${JENV_CANDIDATES[@]:-${JENV_CANDIDATES_DEFAULT[@]}}" | sed -E 's/ /, /g')"
+	echo "   version    :  optional, defaults to latest stable if not provided"
+	echo ""
+	echo "eg: jenv install groovy"
+}
