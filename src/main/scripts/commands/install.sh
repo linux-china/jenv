423e99016d53a7f2043a1c2f8fa4a9278251d392
Jacky Chan <libing.chen@gmail.com>
2013-02-17 20:50:30 +0800

    调整安装逻辑

--- src/main/scripts/commands/install.sh
+++ src/main/scripts/commands/install.sh
@@ -21,13 +21,9 @@ JENV_VERSION="@JENV_VERSION@"
 JENV_DIR="$HOME/.jenv"
 
 # Local variables
-jenv_src_folder="${JENV_DIR}/src"
 jenv_tmp_folder="${JENV_DIR}/tmp"
-jenv_stage_folder="${jenv_tmp_folder}/stage"
-jenv_zip_file="${jenv_tmp_folder}/res-${JENV_VERSION}.zip"
+jenv_zip_file="${jenv_tmp_folder}/jenv-${JENV_VERSION}.zip"
 jenv_ext_folder="${JENV_DIR}/ext"
-jenv_etc_folder="${JENV_DIR}/etc"
-jenv_config_file="${jenv_etc_folder}/config"
 jenv_bash_profile="${HOME}/.bash_profile"
 jenv_profile="${HOME}/.profile"
 jenv_bashrc="${HOME}/.bashrc"
@@ -36,7 +32,7 @@ jenv_platform=$(uname -o)
 
 jenv_init_snippet=$( cat << EOF
 #THIS MUST BE AT THE END OF THE FILE FOR JENV TO WORK!!!
-[[ -s "${JENV_DIR}/src/jenv-init.sh" && -z \$(which jenv-init.sh | grep '/jenv-init.sh') ]] && source "${JENV_DIR}/src/jenv-init.sh"
+[[ -s "${JENV_DIR}/bin/jenv-init.sh" && -z \$(which jenv-init.sh | grep '/jenv-init.sh') ]] && source "${JENV_DIR}/bin/jenv-init.sh"
 EOF
 )
 
@@ -90,40 +86,6 @@ if [ -d "${JENV_DIR}" ]; then
 	exit 0
 fi
 
-echo "Looking for JAVA_HOME..."
-if [ -z "${JAVA_HOME}" ]; then
-	echo "Not found."
-	echo ""
-	echo "======================================================================================================"
-	echo " Please ensure that you have a Java SDK installed and that JAVA_HOME environment variable is set."
-	echo " accordingly."
-	echo ""
-	echo " Java can be found here:"
-	echo "     http://www.oracle.com/technetwork/java/javase/downloads/index.html"
-	echo ""
-	echo " Set JAVA_HOME by editing your ~/.profile file and adding:"
-	echo "     export JAVA_HOME=\"/path/to/my/jdk\""
-	echo "======================================================================================================"
-	echo ""
-	exit 0
-fi
-
-echo "Validating JAVA_HOME..."
-if [ ! -f "${JAVA_HOME}/bin/java" ]; then
-	echo "Invalid."
-	echo ""
-	echo "======================================================================================================"
-	echo " Please ensure that your JAVA_HOME points to a valid Java SDK."
-	echo " You are currently pointing to:"
-	echo ""
-	echo "  ${JAVA_HOME}"
-	echo ""
-	echo " This does not seem to be valid. Please rectify and restart."
-	echo "======================================================================================================"
-	echo ""
-	exit 0	
-fi
-
 echo "Looking for unzip..."
 if [ -z $(which unzip) ]; then
 	echo "Not found."
@@ -153,40 +115,17 @@ fi
 
 echo "Installing jenv scripts..."
 
-
-# Create directory structure
-
-echo "Create distribution directories..."
-mkdir -p "${jenv_src_folder}"
 mkdir -p "${jenv_tmp_folder}"
-mkdir -p "${jenv_stage_folder}"
-mkdir -p "${jenv_ext_folder}"
-mkdir -p "${jenv_etc_folder}"
-
-echo "Create candidate directories..."
-mkdir -p "${JENV_DIR}/groovy"
-mkdir -p "${JENV_DIR}/grails"
-mkdir -p "${JENV_DIR}/griffon"
-mkdir -p "${JENV_DIR}/gradle"
-mkdir -p "${JENV_DIR}/vertx"
-
-echo "Prime the config file..."
-touch "${jenv_config_file}"
-echo "jenv_auto_answer=false" >> "${jenv_config_file}"
-
 echo "Download script archive..."
 curl -s "${JENV_SERVICE}/res?platform=${jenv_platform}&purpose=install" > "${jenv_zip_file}"
 
 echo "Extract script archive..."
 if [[ "${cygwin}" == 'true' ]]; then
 	echo "Cygwin detected - normalizing paths for unzip..."
-	unzip -qo $(cygpath -w "${jenv_zip_file}") -d $(cygpath -w "${jenv_stage_folder}")
+	unzip -qo $(cygpath -w "${jenv_zip_file}") -d "${JENV_DIR}"
 else
-	unzip -qo "${jenv_zip_file}" -d "${jenv_stage_folder}"
+	unzip -qo "${jenv_zip_file}" -d "${JENV_DIR}"
 fi
-
-echo "Install scripts..."
-mv "${jenv_stage_folder}"/jenv-* "${jenv_src_folder}"
 
 echo "Attempt update of bash profiles..."
 if [ ! -f "${jenv_bash_profile}" -a ! -f "${jenv_profile}" ]; then
