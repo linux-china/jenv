f702390639f40251e2f8504bfaf51eade3a89625
Jacky Chan <libing.chen@gmail.com>
2013-02-17 11:51:52 +0800

    jenv default command

---
+++ src/main/resources/scripts/jenv-default.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+#   Copyright 2012 Jacky Chan
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+function __jenvtool_default {
+	CANDIDATE="$1"
+	__jenvtool_check_candidate_present "${CANDIDATE}" || return 1
+	__jenvtool_determine_version "$2" || return 1
+
+	if [ ! -d "${JENV_DIR}/${CANDIDATE}/${VERSION}" ]; then
+		echo ""
+		echo "Stop! ${CANDIDATE} ${VERSION} is not installed."
+		return 1
+	fi
+
+	__jenvtool_link_candidate_version "${CANDIDATE}" "${VERSION}"
+
+	echo ""
+	echo "Default ${CANDIDATE} version set to ${VERSION}"
+}
