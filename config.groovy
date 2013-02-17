e339a2824da7092f48b8737e9adb90638b2cbcb3
Jacky Chan <libing.chen@gmail.com>
2013-02-17 11:38:42 +0800

    调整名称

--- config.groovy
+++ config.groovy
@@ -2,12 +2,12 @@ vertxVersion = '1.3.0.final'
 jenvVersion = '0.0.1'
 environments {
 	dev {
-		gvmService = 'http://localhost:8080'
+		jenvService = 'http://localhost:8080'
 	}
 	test {
-		gvmService = 'http://test.jvmtool.net'
+		jenvService = 'http://test.jvmtool.net'
 	}
 	prod {
-		gvmService = 'http://get.jvmtool.net'
+		jenvService = 'http://get.jvmtool.net'
 	}
 }
