6c9fa8ac70590a51f001cb521677e0bd9009822c
Jacky Chan <libing.chen@gmail.com>
2013-02-18 18:08:25 +0800

    minor update

--- config.groovy
+++ config.groovy
@@ -1,7 +1,7 @@
 jenvVersion = '0.0.1'
 environments {
 	dev {
-		jenvService = 'http://localhost:8080'
+		jenvService = 'http://get.jvmtool.mvnsearch.org'
 	}
 	test {
 		jenvService = 'http://test.jvmtool.mvnsearch.org'
