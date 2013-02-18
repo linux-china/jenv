69874e9cd141feb3f300f6d88dd9934e69ccba14
Jacky Chan <libing.chen@gmail.com>
2013-02-18 19:57:45 +0800

    minor bug fix

--- src/main/scripts/bin/jenv-init.sh
+++ src/main/scripts/bin/jenv-init.sh
@@ -74,7 +74,7 @@ function __jenvtool_init {
         if ! __jenvtool_contains "$PATH" "candidates/${CANDIDATE}/current" && [ -e "${JENV_DIR}/candidates/${CANDIDATE}/current" ]; then
            UPPER_CANDIDATE=`echo "${CANDIDATE}" | tr '[:lower:]' '[:upper:]'`
            export "${UPPER_CANDIDATE}_HOME"="${JENV_DIR}/candidates/${CANDIDATE}/current"
-           if [ -e "${JENV_DIR}/candidates/${CANDIDATE}/current/bin" ]; then
+           if [ -d "${JENV_DIR}/candidates/${CANDIDATE}/current/bin" ]; then
                PATH="${JENV_DIR}/candidates/${CANDIDATE}/current/bin:$PATH"
            else
                PATH="${JENV_DIR}/candidates/${CANDIDATE}/current:$PATH"
