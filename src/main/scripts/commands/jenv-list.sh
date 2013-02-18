8942626d42256f919f9ead7b8b96ebd89f65841b
Jacky Chan <libing.chen@gmail.com>
2013-02-19 00:26:34 +0800

    minor update

--- src/main/scripts/commands/jenv-list.sh
+++ src/main/scripts/commands/jenv-list.sh
@@ -21,13 +21,14 @@
 function __jenvtool_list {
 	CANDIDATE=`echo "$1" | tr '[:upper:]' '[:lower:]'`
 	__jenvtool_check_candidate_present "${CANDIDATE}" || return 1
-	__jenvtool_build_version_csv "${CANDIDATE}"
 	__jenvtool_determine_current_version "${CANDIDATE}"
     CANDIDATE_VERSIONS=($(cat "${JENV_DIR}/db/${CANDIDATE}.txt"))
+    INSTALLED_VERSIONS=()
     # add local unversioned in repository
     for version in $(ls -1 "${JENV_DIR}/candidates/${CANDIDATE}" 2> /dev/null); do
     	if [ ${version} != 'current' ]; then
-             if ! __jenvtool_contains "${CANDIDATE_VERSIONS[*]}" "${version}"; then
+    	      INSTALLED_VERSIONS+=("${version}")
+             if ! __jenvtool_array_contains CANDIDATE_VERSIONS[@] "${version}"; then
                CANDIDATE_VERSIONS+=("${version}")
              fi
         fi
@@ -37,7 +38,7 @@ function __jenvtool_list {
     for candidate_version in "${CANDIDATE_VERSIONS[@]}" ; do
      if [[ "${candidate_version}" == "${CURRENT}" ]]; then
           echo ">* ${candidate_version}"
-     elif __jenvtool_contains "${CSV}" "${candidate_version}"; then
+     elif __jenvtool_array_contains INSTALLED_VERSIONS[@] "${candidate_version}"; then
           echo " * ${candidate_version}"
      else
           echo "   ${candidate_version}"
