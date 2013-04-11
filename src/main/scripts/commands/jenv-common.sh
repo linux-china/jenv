e5bae0382c39b2c0df67d0bff0d4c3a76770ea17
Jacky Chan <libing.chen@gmail.com>
2013-04-11 14:49:05 +0800

    adjust download logic

--- src/main/scripts/commands/jenv-common.sh
+++ src/main/scripts/commands/jenv-common.sh
@@ -97,29 +97,12 @@ function __jenvtool_determine_current_version {
 function __jenvtool_download {
 	CANDIDATE="$1"
 	VERSION="$2"
+	DOWNLOAD_URL="$3"
 	mkdir -p "${JENV_DIR}/archives"
 	if [ ! -f "${JENV_DIR}/archives/${CANDIDATE}-${VERSION}.zip" ]; then
 		echo ""
 		echo "Downloading: ${CANDIDATE} ${VERSION}"
 		echo ""
-		DOWNLOAD_URL="${JENV_SERVICE}/download/${CANDIDATE}/${CANDIDATE}-${VERSION}.zip?platform=${JENV_PLATFORM}"
-		# java candidate logic
-		if [[ "${CANDIDATE}" == "java" || "${CANDIDATE}" == "android" || "${CANDIDATE}" == "mat" || "${CANDIDATE}" == "jad" ]]; then
-		   suffix=""
-		   if [ "$darwin" = "true" ] ; then
-		      suffix="-darwin"
-		    else
-		      if [[ "${JENV_MACHINE_PLATFORM}" == "x86_64" ]]; then
-		          if [[ "${CANDIDATE}" == "java" || "${CANDIDATE}" == "mat"  ]]; then
-		             suffix="-x64"
-		          fi
-		      fi
-		      if [ "$cygwin" = "true" ] ; then
-		          suffix="${suffix}-win"
-		      fi
-		   fi
-		   DOWNLOAD_URL="${JENV_SERVICE}/download/${CANDIDATE}/${CANDIDATE}-${VERSION}${suffix}.zip"
-		fi
 		ZIP_ARCHIVE="${JENV_DIR}/archives/${CANDIDATE}-${VERSION}.zip"
 		echo "Downloading ${DOWNLOAD_URL}"
 		curl -L "${DOWNLOAD_URL}" > "${ZIP_ARCHIVE}"
