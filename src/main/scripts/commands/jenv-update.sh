3b386e5574d339822f17762e5494a75f1a146ea8
Jacky Chan <libing.chen@gmail.com>
2013-04-17 13:39:42 +0800

    add update all git and svn based candidate

--- src/main/scripts/commands/jenv-update.sh
+++ src/main/scripts/commands/jenv-update.sh
@@ -20,6 +20,28 @@
 # @param $1 candidate name
 # @param $2 candidate version
 function __jenvtool_update {
+    # update all candidates
+    if [[ -z "$1" ]]; then
+        for i in "$HOME/.jenv/candidates"/*;do
+            if [ -d "$i" ];then
+               for j in "$i"/*;do
+                 if ! __jenvtool_contains "$j" "current"; then
+                   if [ -d "$j/.git" ];then
+                        echo "Pulling..."
+                        (cd "${j}" && git pull)
+                        __jenvtool_echo_green "${j} has been updated!"
+                   elif [ -d "$j/.svn" ];then
+                        echo "Updating..."
+                        (cd "${j}" && svn update)
+                        __jenvtool_echo_green "${j} has been updated!"
+                   fi
+                 fi
+                done
+            fi
+         done
+         return 0;
+    fi
+    # update candidate with version
 	CANDIDATE=`echo "$1" | tr '[:upper:]' '[:lower:]'`
     VERSION="$2"
     if [[ -z "${VERSION}" ]]; then
@@ -30,7 +52,7 @@ function __jenvtool_update {
         (cd "${JENV_DIR}/candidates/${CANDIDATE}/${VERSION}/" && git pull)
         __jenvtool_echo_green "${CANDIDATE}'s ${VERSION} has been updated!"
      elif [ -d "${JENV_DIR}/candidates/${CANDIDATE}/${VERSION}/.svn" ]; then
-         echo "Updating..."
+        echo "Updating..."
         (cd "${JENV_DIR}/candidates/${CANDIDATE}/${VERSION}/" && svn update)
          __jenvtool_echo_green "${CANDIDATE}'s ${VERSION} has been updated!"
      else
