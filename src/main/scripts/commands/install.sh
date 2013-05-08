1d63a9166f24385e520c6db465b060fcd7086c52
Jacky Chan <libing.chen@gmail.com>
2013-05-08 22:20:40 +0800

    copyright update

--- src/main/scripts/commands/install.sh
+++ src/main/scripts/commands/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#   Copyright 2012 Jacky Chan
+#   Copyright 2013 Jacky Chan
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
