39fa5c8e84de7919f4f6f0e29ea7c1a63eaad344
khotyn <hting1@gmail.com>
2013-06-18 18:33:03 +0800

    ZSH completion support.

---
+++ src/main/scripts/commands/jenv-zsh-completion.zsh
@@ -0,0 +1,58 @@
+if [[ ! -o interactive ]]; then
+    return
+fi
+
+compctl -K _jenv jenv
+
+_jenv_commands()
+{
+    cmds=(all ls candidates list update install uninstall pause execute exe use which cd version default add repo selfupdate help requirements)
+	for cmd in ${cmds}
+	do
+	    echo $cmd
+	done
+}
+
+_jenv_candidates()
+{
+    for candidate in ${JENV_CANDIDATES}
+    do
+        echo $candidate
+    done
+}
+
+_jenv_repo()
+{
+    echo "add"
+    echo "update"
+}
+
+_jenv() {
+    local words completions
+    read -cA words
+
+    if [ "${#words}" -eq 2 ]; then
+        completions="$(_jenv_commands)"
+    else
+        typeset prev
+        prev=${words[2, -2]}
+
+        case "${prev}" in
+        use)        completions="$(_jenv_candidates)";;
+        pause)      completions="$(_jenv_candidates)";;
+        list)       completions="$(_jenv_candidates)";;
+        ls)         completions="$(_jenv_candidates)";;
+        install)    completions="$(_jenv_candidates)";;
+        execute)    completions="$(_jenv_candidates)";;
+        exe)        completions="$(_jenv_candidates)";;
+        show)       completions="$(_jenv_candidates)";;
+        default)    completions="$(_jenv_candidates)";;
+        which)      completions="$(_jenv_candidates)";;
+        cd)         completions="$(_jenv_candidates)";;
+        uninstall)  completions="$(_jenv_candidates)";;
+        repo)       completions="$(_jenv_repo)";;
+        *) completions=();;
+        esac
+    fi
+    reply=("${(ps:\n:)completions}")
+}
